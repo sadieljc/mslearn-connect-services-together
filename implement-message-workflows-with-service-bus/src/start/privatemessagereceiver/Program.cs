bff9e51dc07a267fb389948f2d33504985d7dc08
Sadiel Jarvis <sadieljc@hotmail.com>
2021-11-01 13:22:46 -0400

    Updated private message sender and receiver code files

--- implement-message-workflows-with-service-bus/src/start/privatemessagereceiver/Program.cs
+++ implement-message-workflows-with-service-bus/src/start/privatemessagereceiver/Program.cs
@@ -1,60 +1,63 @@
 ﻿using System;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Azure.ServiceBus;
+using Azure.Messaging.ServiceBus;
 
 namespace privatemessagereceiver
 {
     class Program
     {
 
-        const string ServiceBusConnectionString = "";
+        const string ServiceBusConnectionString = "Endpoint=sb://salesteamapp-sjc-nov1st.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=XDr0MV/cvznAqnhaAkEJlmYrH2+6sxEteEIDPIxBHM4=";
         const string QueueName = "salesmessages";
-        static IQueueClient queueClient;
 
         static void Main(string[] args)
         {
-
             ReceiveSalesMessageAsync().GetAwaiter().GetResult();
-
         }
 
         static async Task ReceiveSalesMessageAsync()
         {
-
-            // Create a Queue Client here
-
             Console.WriteLine("======================================================");
             Console.WriteLine("Press ENTER key to exit after receiving all the messages.");
             Console.WriteLine("======================================================");
 
-            RegisterMessageHandler();
-        
+            // Create a Queue Client here
+            var client = new ServiceBusClient(ServiceBusConnectionString);
+
+            var processorOptions = new ServiceBusProcessorOptions
+            {
+                MaxConcurrentCalls = 1,
+                AutoCompleteMessages = false
+            };
+
+            ServiceBusProcessor processor = client.CreateProcessor(QueueName, processorOptions);
+
+            processor.ProcessMessageAsync += ProcessMessagesAsync;
+            processor.ProcessErrorAsync += ExceptionReceivedHandler;
+
+            await processor.StartProcessingAsync();
+            
             Console.Read();
 
             // Close the queue here
-
+            await processor.DisposeAsync();
+            await client.DisposeAsync();
         }
 
-        static void RegisterMessageHandler()
+        static async Task ProcessMessagesAsync(ProcessMessageEventArgs args)
         {
-            throw new NotImplementedException();
+            Console.WriteLine($"Received message: SequenceNumber:{args.Message.SequenceNumber} Body:{args.Message.Body}");
+            await args.CompleteMessageAsync(args.Message);
         }
 
-        static async Task ProcessMessagesAsync(Message message, CancellationToken token)
-        {
-            throw new NotImplementedException();
-        }
-
-        static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        static Task ExceptionReceivedHandler(ProcessErrorEventArgs exceptionReceivedEventArgs)
         {
             Console.WriteLine($"Message handler encountered an exception {exceptionReceivedEventArgs.Exception}.");
-            var context = exceptionReceivedEventArgs.ExceptionReceivedContext;
             Console.WriteLine("Exception context for troubleshooting:");
-            Console.WriteLine($"- Endpoint: {context.Endpoint}");
-            Console.WriteLine($"- Entity Path: {context.EntityPath}");
-            Console.WriteLine($"- Executing Action: {context.Action}");
+            Console.WriteLine($"- Endpoint: {exceptionReceivedEventArgs.FullyQualifiedNamespace}");
+            Console.WriteLine($"- Entity Path: {exceptionReceivedEventArgs.EntityPath}");
+            Console.WriteLine($"- Executing Action: {exceptionReceivedEventArgs.ErrorSource}");
             return Task.CompletedTask;
         }   
     }
