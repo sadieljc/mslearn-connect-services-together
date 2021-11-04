4d3e469985998cda43f43deb67e167b4eae1b87e
Sadiel Jarvis <sadieljc@hotmail.com>
2021-11-04 17:30:55 -0400

    Updated topic sender and receiver code

--- implement-message-workflows-with-service-bus/src/start/performancemessagesender/Program.cs
+++ implement-message-workflows-with-service-bus/src/start/performancemessagesender/Program.cs
@@ -1,43 +1,39 @@
 ﻿using System;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Azure.ServiceBus;
+using Azure.Messaging.ServiceBus;
 
 namespace performancemessagesender
 {
     class Program
     {
-        const string ServiceBusConnectionString = "";
+        const string ServiceBusConnectionString = "Endpoint=sb://salesteamapp-sjc-nov4th.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=OttwzqMcO85mohyGkBI++56o5d97qo9l+UUoMCPVN24=";
         const string TopicName = "salesperformancemessages";
-        static ITopicClient topicClient;
 
         static void Main(string[] args)
         {
-
             Console.WriteLine("Sending a message to the Sales Performance topic...");
-
             SendPerformanceMessageAsync().GetAwaiter().GetResult();
-
             Console.WriteLine("Message was sent successfully.");
-
         }
 
         static async Task SendPerformanceMessageAsync()
         {
             // Create a Topic Client here
+            await using var client = new ServiceBusClient(ServiceBusConnectionString);
+            await using ServiceBusSender sender = client.CreateSender(TopicName);
 
             // Send messages.
             try
             {
-                // Create and send a message here
+                var messageBody = "Total sales for Brazil in August: $13m.";
+                var message = new ServiceBusMessage(messageBody);
+                Console.WriteLine($"Sending message: {messageBody}");
+                await sender.SendMessageAsync(message);
             }
             catch (Exception exception)
             {
                 Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
             }
-
-            // Close the connection to the topic here
         }
     }
 }
